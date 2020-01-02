7b0e6509af2b933b92b3d25d2027b4ec48f0975c
cuilibao <cuilibao123@gmail.com>
2020-01-02 19:43:13 +0800

    tree

--- 数据结构/树/BinarySearchTree.js
+++ 数据结构/树/BinarySearchTree.js
@@ -29,7 +29,76 @@ class BinarySearchTree {
         this._insertNode(node.right, newNode);
       }
     }
+  }
 
+  max() {
+    let node = this.root;// 30
+    while (node.right) {
+      node = node.right;
+    }
+    return node.key;
+  }
+
+  min() {
+    let node = this.root;// 30
+    while (node.left) {
+      node = node.left;
+    }
+    return node.key;
+  }
+
+  search(key) {
+    let node = this.root;
+    // while (node) {
+    //   if (node.key > key) {
+    //     if (node.left) {
+    //       node = node.left;
+    //     } else {
+    //       return false;
+    //     }
+    //   } else if (node.key < key) {
+    //     if (node.right) {
+    //       node = node.right;
+    //     } else {
+    //       return false;
+    //     }
+    //   } else {
+    //     return true;
+    //   }
+    // }
+    return this._searchNode(node, key)
+  }
+
+
+  _searchNode(node, key) {
+    if (!node) {
+      return false;
+    }
+    if (node.key > key) {
+      return this._searchNode(node.left, key);
+    } else if (node.key < key) {
+      return this._searchNode(node.right, key);
+    } else return node.key === key;
+  }
+
+  /*
+  * 排序: 二分 查找
+  *
+  * 先
+  * 中序遍历
+  * 后
+  * */
+  midTraverse(handler) {
+    let node = this.root;
+    this._midTraverse(node, handler);
+  }
+
+  _midTraverse(node, handler) {
+    if (node) {
+      this._midTraverse(node.left, handler);
+      handler(node.key);
+      this._midTraverse(node.right, handler);
+    }
   }
 }
 
